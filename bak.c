0376ea1724febfc62405ad66af5e9459a9df2894
sowmya <sowcat@sowmya.(none)>
2012-10-08 11:57:10 +0530

    Bakery

---
+++ bak.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define N     10
+#define TRUE    1
+#define FALSE   0
+
+int global = 10;
+int entering[N];
+int number[N];
+
+int max(int number[N]) {
+    int i = 0;
+    int maximum = number[0];
+    for (i = 0; i < N; i++) {
+        if (maximum < number[i])
+            maximum = number[i];
+        }
+    return maximum;
+    }
+
+int main(int argc, char ** argv) {
+    int iret[N];
+    void *function(void *);
+    pthread_t thread[N];
+    int i = 0; 
+    for (i = 0; i < N; i++) {
+        iret[i] = pthread_create(&thread[i], NULL, function, (void *)i); 
+        pthread_join(thread[i], NULL);
+    }
+    return EXIT_SUCCESS;
+}
+
+void lock(int i) {
+    int j = 0;
+    entering[i] = TRUE;
+    number[i] = 1 + max(number);
+    entering[i] = FALSE;
+    for (j = 0; j < N; j++) {
+        while (entering[j]);
+        while (number[j] != 0 && (number[j] < number[i] || (number[i] == number[j]) && j < i)) {}
+    }
+}
+
+void unlock(int i) {
+    number[i] = 0;
+}
+
+void *function(void *integer) {
+    int i = (int) integer;
+    lock(i);
+    global++;
+    printf("%d is the value of global \n",global);
+    
+    unlock(i);
+}    
+
